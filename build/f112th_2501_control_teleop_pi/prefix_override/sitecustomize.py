b0650ac8b9f60b336b18a8f6e48b1a83757f5f84
Daniel <dancorpa@gmail.com>
2024-12-23 16:49:17 -0500

    used colcon build to create all necessary folders

---
+++ build/f112th_2501_control_teleop_pi/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/daniel/robotica/f112th_2501_camel_rpi4/install/f112th_2501_control_teleop_pi'
