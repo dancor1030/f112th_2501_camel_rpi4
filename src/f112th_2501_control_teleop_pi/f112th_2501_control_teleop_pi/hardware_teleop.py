1ce3998bed9f43d4d97016d5a39755c1fac4800c
Daniel <dancorpa@gmail.com>
2024-12-23 22:41:50 -0500

    added a node structure for hw_teleop node

--- src/f112th_2501_control_teleop_pi/f112th_2501_control_teleop_pi/hardware_teleop.py
+++ src/f112th_2501_control_teleop_pi/f112th_2501_control_teleop_pi/hardware_teleop.py
@@ -1,3 +1,20 @@
+import rclpy
+import numpy as np
+from geometry_msgs.msg import Twist
+from rclpy.node import Node
+
+class HW_TELEOP(Node):
+    def __init__(self):
+        super().__init__("hardware_teleop_node")
+
+        self.sub = self.create_subscription(Twist, "cmd_vel", self.pwm_response)
+
+    def pwm_response(self, data):
+        self.thrust = data.linear.x
+        self.angle = data.angular.x
+
+    # def 
+
 def main():
     print('Hi from f112th_2501_control_teleop_pi.')
 
