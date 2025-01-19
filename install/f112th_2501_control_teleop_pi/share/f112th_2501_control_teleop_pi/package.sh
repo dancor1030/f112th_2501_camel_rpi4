a9b77ae8a9e6de6bc8fef39fe8e58b8e537dd120
Camel <raspberryrozo@gmail.com>
2025-01-18 23:16:15 -0500

    changed maintainer's info for a generic maintainer

--- install/f112th_2501_control_teleop_pi/share/f112th_2501_control_teleop_pi/package.sh
+++ install/f112th_2501_control_teleop_pi/share/f112th_2501_control_teleop_pi/package.sh
@@ -79,7 +79,6 @@ _colcon_package_sh_source_script() {
 
 # source sh hooks
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/f112th_2501_control_teleop_pi/hook/pythonpath.sh"
-_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/../../build/f112th_2501_control_teleop_pi/share/f112th_2501_control_teleop_pi/hook/pythonpath_develop.sh"
 
 unset _colcon_package_sh_source_script
 unset COLCON_CURRENT_PREFIX
