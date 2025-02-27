9e28f3de7b01e5f7ea55469054f3fa20f5b30ee3
dancor1030 <dancorpa@gmail.com>
2025-02-27 14:47:24 -0500

    added conditionals for deadman switch

--- src/f112th_2501_control_teleop_pi/f112th_2501_control_teleop_pi/hardware_teleop.py
+++ src/f112th_2501_control_teleop_pi/f112th_2501_control_teleop_pi/hardware_teleop.py
@@ -17,26 +17,33 @@ class HW_TELEOP(Node):
         super().__init__("hardware_teleop_node")
 
         self.sub = self.create_subscription(Joy, "joy", self.act, 10)
+        self.dir = 90
+        self.thrust = 90
         # self.pub = self.create_publisher()
 
     def act(self, data):
         self.deadman = data.buttons[4]
-        # 'remote' means it is the value obtained from the remote controller
         self.remotedir = data.axes[0]
         self.remotethrust = data.axes[5]
-        self.reverse = data.buttons[3]
+        self.reverse = data.buttons[3]        
+        #! 'remote' means it is the value obtained from the remote controller
 
-        self.dir = map(self.remotedir, 0, 90, 1, 135)
-        if self.reverse == 0:
-            self.thrust = map(self.remotethrust, 1, 90, -1, 120)
+        if self.deadman == 0:
+            print('deadman is OFF')
+            ## STOP EVERYTHING
+            bot.set_pwm_servo(1, 90)
+            bot.set_pwm_servo(2, 90)
+            ## STOP EVERYTHING
         else:
-            self.thrust = map(self.remotethrust, 1, 90, -1, 75)
+            self.dir = map(self.remotedir, 0, 90, 1, 135)
+            if self.reverse == 0:
+                self.thrust = map(self.remotethrust, 1, 90, -1, 120)
+            else:
+                self.thrust = map(self.remotethrust, 1, 90, -1, 75)
 
-
-        print(f'dir: {self.dir} | thrust: {self.thrust}')
-        
-        bot.set_pwm_servo(1, self.dir)
-        bot.set_pwm_servo(2, self.thrust)
+            print(f'dir: {self.dir} | thrust: {self.thrust}')        
+            bot.set_pwm_servo(1, self.dir)
+            bot.set_pwm_servo(2, self.thrust)
 
 
 def main(args=None):
