13baa44c029d17ee5b9c89a1dd0d05d6347bd162
dancor1030 <dancorpa@gmail.com>
2025-03-03 16:15:12 -0500

    changed launch file for it to work

--- src/f112th_2501_control_teleop_pi/launch/rplidar.launch.py
+++ src/f112th_2501_control_teleop_pi/launch/rplidar.launch.py
@@ -9,10 +9,14 @@ def generate_launch_description():
                     executable = 'rplidar_composition',
                     output = 'screen',
                     parameters = [{
+                        # 'serial_port': '/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0',
                         'serial_port': '/dev/ttyUSB0',
-                        'frame_id': 'laser_frame',
+                        'serial_baudrate' : 115200,
+                        'frame_id': 'laser',
+                        'inverted' : False,
                         'angle_compensate': True,
                         'scan_mode': 'Standard'
+                        # 'scan_mode': 'Sensitivity'
                     }])
 
 
--- src/f112th_2501_control_teleop_pi/setup.py
+++ src/f112th_2501_control_teleop_pi/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'f112th_2501_control_teleop_pi'
 
@@ -8,8 +10,10 @@ setup(
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
+            # [package_name]),
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
