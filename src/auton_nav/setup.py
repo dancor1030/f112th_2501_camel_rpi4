c0d79a95789c4f6abb87fea6f6060ceebf41cdab
dancor1030 <dancorpa@gmail.com>
2025-06-03 12:11:52 -0500

    copied all the nodes and launch files and configs from the sim repo

---
+++ src/auton_nav/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'auton_nav'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.[yaml]*')))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='daniel',
+    maintainer_email='dancorpa@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'a_star = auton_nav.a_star:main',
+            'pure_pursuit = auton_nav.pure_pursuit:main',
+        ],
+    },
+)
